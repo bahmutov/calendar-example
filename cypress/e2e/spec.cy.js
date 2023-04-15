9cfafc4a4d8ccb1f5eb0cf70eeb1c885f94b3064
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-04-15 12:04:29 -0400

    more spec

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,9 +6,8 @@ describe('test calendart', () => {
   })
 
   it('Find and print how many empty fields there are for each month', () => {
-    cy.visit('/kalendar-2022-srb.php')
     Cypress.env('total', 0)
-    cy.get('table').each(($tables) => {
+    cy.get('table.month').each(($tables) => {
       const name = $tables.find('.month-title').text()
       const empty = $tables.find('tbody td:empty').length
       cy.log(`The number of empty fields is ${empty} for month ${name}`)
@@ -17,15 +16,9 @@ describe('test calendart', () => {
   })
 
   it('Count and print how many empty fields there are for entire year', () => {
-    cy.visit('/kalendar-2022-srb.php')
-    cy.get('td.day.new').its('length').as('new')
-    cy.get('td.day.old')
+    cy.get('td.day.new,td.day.old')
+      .should('be.visible')
       .its('length')
-      .as('old')
-      .then(function () {
-        expect(this.new + this.old, 'year to month').to.equal(
-          Cypress.env('total'),
-        )
-      })
+      .should('equal', Cypress.env('total'))
   })
 })
